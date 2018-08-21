f172d46f4352aaaff6dba613dc7dbf7617e7ea9f
matovu-ronald <maronwhite6@gmail.com>
2018-08-21 17:51:06 +0300

    Finished Working with forms

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,121 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+    <div class="container">
+        <div class="col-xs-12">
+            <h1>Add Blog Post</h1>
+            <form action="#" @submit.prevent="isSubmitted = true">
+                <div class="form-group">
+                    <label for="title">Title</label>
+                    <input type="text" class="form-control" id="title" placeholder="Title" v-model.trim="post.title">
+                </div>
+                <div class="form-group">
+                    <label for="content">Content</label>
+                    <textarea id="content" cols="30" rows="10" class="form-control"
+                              v-model.lazy.trim="post.content"></textarea>
+                </div>
+                <div class="form-group">
+                    <input type="checkbox" id="publish-immediately" v-model="post.publishImmediately" true-value="immediately" false-value="later">
+                    <label for="publish-immediately">Publish Immediately</label>
+                </div>
+                <div class="form-group">
+                    <label >Share On</label>
+                    <div v-for="media in formData.socialMedia">
+                        <input type="checkbox" :id="media" :value="media" v-model="post.shareOn">
+                        <label :for="media">{{ media }}</label>
+                    </div>
+                </div>
+                <div class="form-group">
+                    <label >Share On</label>
+                    <div v-for="category in formData.categories">
+                        <input type="radio" :id="category" :value="category" v-model="post.category" >
+                        <label :for="category">{{ category }}</label>
+                    </div>
+                </div>
+                <div class="form-group">
+                    <select  id="select-series" class="form-control" v-model="post.series">
+                        <option value="">Choose Series</option>
+                        <option v-for="series in formData.series" :value="series"> {{ series }} </option>
+                    </select>
+                </div>
+                <input type="submit" class="btn btn-primary" value="published">
+            </form>
+            <hr>
+            <table class="table table-stripped table-hover" v-if="isSubmitted">
+                <thead>
+                    <tr>
+                        <td class="col-xs-6"><strong>Field</strong></td>
+                        <td class="col-xs-6"><strong>Value</strong></td>
+
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td>Title</td>
+                        <td>{{ post.title }}</td>
+                    </tr>
+                    <tr>
+                        <td>Content</td>
+                        <td style="white-space: pre;">{{ post.content }}</td>
+                    </tr>
+                    <tr>
+                        <td>Publish Immediately</td>
+                        <td>{{ post.publishImmediately }}</td>
+                    </tr>
+                    <tr>
+                        <td>Share on</td>
+                        <td>
+                            <ul>
+                                <li v-for="media in post.shareOn">{{ media }}</li>
+                            </ul>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>Category</td>
+                        <td>{{ post.category }}</td>
+                    </tr>
+                    <tr>
+                        <td>Series</td>
+                        <td>{{ post.series }}</td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+    </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'app',
-  components: {
-    HelloWorld
+  data() {
+      return {
+          isSubmitted: false,
+          post: {
+              title: '',
+              content: '',
+              publishImmediately: false,
+              shareOn: [],
+              category: '',
+              series: ''
+          },
+          formData: {
+              socialMedia: ['facebook', 'Twitter' ],
+              categories: ['Frontend', 'Backend'],
+              series: [
+                  'Vue.js: From Beginner to Professional',
+                  'Complete Guide to ElasticSearch'
+              ]
+          }
+
+      };
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+    input[type="radio"] + label,
+    input[type="checkbox"] + label {
+      font-weight: normal;
+    }
+
+    @import '~bootstrap/dist/css/bootstrap.css';
 </style>
